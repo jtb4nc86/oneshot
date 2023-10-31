efdcdeed850211113215abc70ea562fe1697d224
Jason Tong <jason@Jasons-Air.lan>
2023-10-31 19:09:24 -0400

    completed templates, views, urls, views

--- todo_list/todo_app/views.py
+++ todo_list/todo_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 
-from django.views.generic import ListView, CreateView, UpdateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from .models import ToDoList, ToDoItem
 
 # Create your views here.
@@ -73,3 +73,21 @@ class ItemUpdate(UpdateView):
 
     def get_success_url(self):
         return reverse("list", args=[self.object.todo_list_id])
+    
+
+class ListDelete(DeleteView):
+    model = ToDoList
+    
+    success_url = reverse_lazy("index")
+    
+
+class ItemDelete(DeleteView):
+    model = ToDoItem
+
+    def get_success_url(self):
+        return reverse_lazy("list", args=[self.kwargs["list_id"]])
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["todo_list"] = self.object.todo_list
+        return context
